eacf2662a15db41bff3560091eb20f947d5a8b95
Irzal Muhammad Prasetyo <irzalprasetyo11@gmail.com>
2026-05-20 22:26:10 +0700

    update config untuk github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Mode export statis agar bisa jalan di GitHub Pages
+  output: 'export',
+  
+  // Mengabaikan error typescript saat build agar proses lebih lancar
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  // Konfigurasi gambar untuk mode statis
   images: {
     unoptimized: true,
   },
-}
 
-export default nextConfig
+  /* HAPUS komentar pada baris basePath di bawah ini JIKA Anda ingin 
+     mengakses via irzalmuhammadprasetyo.github.io/Dashboard/
+  */
+  // basePath: '/Dashboard',
+};
+
+export default nextConfig;

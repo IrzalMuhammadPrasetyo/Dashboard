e21921d6158fd9557adb3f25c8c73257671ae8fe
Irzal Muhammad Prasetyo <irzalprasetyo11@gmail.com>
2026-05-20 22:48:54 +0700

    fix api route for static export

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getCustomerData } from "@/lib/google-sheets";
 
-//export const dynamic = "force-dynamic";
+export const dynamic = "force-static";
 export const revalidate = 0;
 
 export async function GET() {
